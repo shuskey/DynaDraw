8f167b760cdbb8410d6de5cf8e18d8ca5e71397a
Scott Huskey <scott@photoloom.com>
2021-02-12 15:53:23 -0800

    Display Build Version on screen

---
+++ Assets/Scripts/GetBuildVersion.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetBuildVersion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {        
+        gameObject.GetComponent<Text>().text = $"Version {Application.version}";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
